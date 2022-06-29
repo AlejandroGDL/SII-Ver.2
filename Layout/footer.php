65adc4e56a6b45450bc496811aae4d310a63ef20
Alejandro Guevara de Luna <alejandrogdlcecyt@gmail.com>
2022-06-29 14:12:28 -0500

    1Ver. SII
    Primer versión SII (Base)

---
+++ Layout/footer.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!--CSS-->
+    <link rel="stylesheet" href="/CSS/footertyles.css">
+    <!--Fuente Online-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <title></title>
+  </head>
+  <body>
+
+
+      <footer class="Footer_Container">
+
+        <div class="Contact">
+
+          <p>Correo Electronico:</p>
+          <p>Teléfono:</p>
+
+        </div>
+
+        <div class="Media">
+          <img src="" alt=""></img>
+          <p>Facebook</p>
+
+          <img src="" alt=""></img>
+          <p>Twitter</p>
+
+          <img src="" alt=""></img>
+          <p>Youtube</p>
+        </div>
+
+      </footer>
+
+
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!--CSS-->
+    <link rel="stylesheet" href="/CSS/indexstyles.css">
+    <!--Fuente Online-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <title></title>
+  </head>
+  <body class="body">
+      <!--Encabezado-->
+      <header class="header"></header>
+
+      <!--Barra de Navegación-->
+      <nav class="navbar"><iframe src="header.html"></iframe> <a href="layout/footer.html">Footer</a> </nav>
+      <!--Contenedor Principal-->
+      <article class="main">Content</article>
+
+      <!--Pie de Pagína-->
+      <?php require('./Layout/header.php') ?>
+
+      <footer class="footer">Footer</footer>
+  </body>
+</html>
